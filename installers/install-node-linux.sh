2c052d0876259ad81365d77b6feb998c88e8b8ac
ＴｈｅＦｅｚｉａｋ 🚀💎 <162903090+thefeziak@users.noreply.github.com>
2025-02-25 20:21:37 +0100

    Update install-node-linux.sh

--- installers/install-node-linux.sh
+++ installers/install-node-linux.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo "Script is not ready."
+apt update
+apt install python3
+apt install python3-pip
+
+cd /
+mkdir Panda
+cd Panda
+mkdir Node
+cd Node
+wget https://raw.githubusercontent.com/thefeziak/PandaPanel/refs/heads/main/PandaNode/server.py
+wget https://raw.githubusercontent.com/thefeziak/PandaPanel/refs/heads/main/PandaNode/requirements.txt
+wget https://raw.githubusercontent.com/thefeziak/PandaPanel/refs/heads/main/PandaNode/config.json
+pip install -r requirements.txt
+
+echo "---------- Done ----------"
+echo "Run with python3 server.py"
+echo "---------- Done ----------"
